755e92531e1685db2bfdb90bfd4a689aa96ec185
samufacco <samuel.facchin.sf@gmail.com>
2019-05-13 09:46:45 +0200

    fixed 2

---
+++ deleteToDo.php
@@ -0,0 +1,23 @@
+<?php
+    include 'connection.php';
+
+    $id= $_POST['id'];
+
+    $stmt = $connection->prepare("SELECT * FROM todolist WHERE id=?");
+    $stmt->bind_param("i",$id);
+    $stmt->execute();
+
+    $r = $stmt->get_result();
+    //controllo se esiste 
+    //if($r->num_rows()!=0){
+
+        //elimino riga
+        $stmt = $connection->prepare("DELETE FROM todolist WHERE id=?"); 
+        $stmt->bind_param("i",$id);
+        $stmt->execute();
+    //}
+
+    $stmt->close();
+
+    header("Location: index.php");
+?>
